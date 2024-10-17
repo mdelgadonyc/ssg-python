c86115bbc8215466999efa71de5f58e2e6da47f9
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-16 20:04:49 -0400

    feat: add split_nodes_delimiter function to split text into nodes by delimiter

---
+++ src/markdown_parser.py
@@ -0,0 +1,21 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    for node in old_nodes:
+        if delimiter in node.text:
+            node.text_type = text_type
+            segments = node.text.split(delimiter)
+            new_nodes = []
+
+            text_type_toggle = False
+
+            for segment in segments:
+                if text_type_toggle:
+                    new_nodes.append(TextNode(segment, text_type))
+                    text_type_toggle = False
+                else:
+                    new_nodes.append(TextNode(segment, TextType.TEXT))
+                    text_type_toggle = True
+                
+            return new_nodes
+
