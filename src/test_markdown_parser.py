bcd49225c8383a462c46e3e489ea1bee55a34e20
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-17 13:27:23 -0400

    test: Add unit tests for link and image from markdown extraction methods

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,6 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
-from markdown_parser import split_nodes_delimiter
+from markdown_parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+
 
 class TestMarkdownParser(unittest.TestCase):
     def test_split_nodes_delimiter_code(self):
@@ -34,9 +35,34 @@ class TestMarkdownParser(unittest.TestCase):
             TextNode("italic", TextType.ITALIC),
             TextNode(" word", TextType.TEXT)
         ]
-        self.assertEqual(new_nodes, new_nodes2)    
+        self.assertEqual(new_nodes, new_nodes2)
 
-    
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        extracted_images = extract_markdown_images(text)
+        expected_images = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extracted_images, expected_images)
+
+    def test_extract_markdown_no_images(self):
+        text = "This is text with a missing image"
+        extracted_images = extract_markdown_images(text)
+        expected_images = []
+        self.assertEqual(extracted_images, expected_images)
+        
+
+    def test_extract_markdown_no_alt_text(self):
+        text = "Empty alt: ![](http://example.com/cat.jpg)"
+        extracted_images = extract_markdown_images(text)
+        expected_images = [('', "http://example.com/cat.jpg")]
+        self.assertEqual(extracted_images, expected_images)
+
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        extracted_links = extract_markdown_links(text)
+        expected_links = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extracted_links, expected_links)
+
 
 if __name__ == "__main__":
     unittest.main()
