388a34279c08dda12bd6d3ca34a41d6470f72282
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-17 20:09:47 -0400

    Add methods that extract link and image nodes from textnodes

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -19,6 +19,19 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     text_type_toggle = True
                 
             return new_nodes
+        
+def split_nodes_links(old_nodes) -> list[TextNode]:
+    new_nodes = []
+    links = extract_markdown_links(old_nodes.text)    
+    text_segments = re.split(r"(?<!!)\[(?:[^\[\]]*)\]\((?:[^()]*)\)", old_nodes.text)
+
+    for segment in text_segments[:-1]:
+        new_nodes.append(TextNode(segment, TextType.TEXT))
+        if links:
+            text, url = links.pop(0)
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+
+    return new_nodes
 
 def extract_markdown_images(text):
     return re.findall("!\[([^\[\]]*)\]\(([^()]*)\)", text)
