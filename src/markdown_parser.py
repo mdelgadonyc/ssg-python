f62840f4d5f7e4975cae7b437721940c00d44678
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-17 20:30:27 -0400

    feat: Complete the extract image node from textnode functionality

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -20,7 +20,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 
             return new_nodes
         
-def split_nodes_links(old_nodes) -> list[TextNode]:
+def split_nodes_link(old_nodes) -> list[TextNode]:
     new_nodes = []
     links = extract_markdown_links(old_nodes.text)    
     text_segments = re.split(r"(?<!!)\[(?:[^\[\]]*)\]\((?:[^()]*)\)", old_nodes.text)
@@ -32,6 +32,20 @@ def split_nodes_links(old_nodes) -> list[TextNode]:
             new_nodes.append(TextNode(text, TextType.LINK, url))
 
     return new_nodes
+
+def split_nodes_image(old_nodes) -> list[TextNode]:
+    new_nodes = []
+    images = extract_markdown_images(old_nodes.text)    
+    text_segments = re.split(r"!\[(?:[^\[\]]*)\]\((?:[^()]*)\)", old_nodes.text)
+
+    for segment in text_segments[:-1]:
+        new_nodes.append(TextNode(segment, TextType.TEXT))
+        if images:
+            text, url = images.pop(0)
+            new_nodes.append(TextNode(text, TextType.IMAGE, url))
+
+    return new_nodes
+
 
 def extract_markdown_images(text):
     return re.findall("!\[([^\[\]]*)\]\(([^()]*)\)", text)
