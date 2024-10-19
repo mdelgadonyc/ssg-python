78d358c7e6cd2f255c894634373752cec5e6a6fa
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-19 13:09:07 -0400

    test: Add unit test for the method that splits markdown into logical blocks

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -110,6 +110,22 @@ class TestMarkdownParser(unittest.TestCase):
 
         self.assertEqual(text_nodes, expected_node)
 
-            
+    def test_markdown_to_blocks(self):
+        markdown = """# This is a heading
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+        * This is a list item
+        * This is another list item"""
+
+        expected_blocks = ['# This is a heading', 
+                           'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
+                           '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+                           ]
+
+        block_result = markdown_to_blocks(markdown)
+        self.assertEqual(expected_blocks, block_result)
+
 if __name__ == "__main__":
     unittest.main()
