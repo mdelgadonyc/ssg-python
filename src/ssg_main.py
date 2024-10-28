534f75512a4d1e0488ce2e92ae256a2988e4a63f
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-28 13:40:36 -0400

    feat: Enhance markdown parsing and HTML generation for static site generator
    - Integrated `markdown_to_html_node` function to parse markdown text into structured HTML nodes
    - Improved handling of various markdown block types, including headers, paragraphs, quotes, and lists
    - Added helper functions (`header_helper`, `normaltext_helper`, `blockquote_helper`, etc.) to manage specific text transformations
    - Utilized regular expressions in `create_list_items` for robust list item parsing
    - Streamlined nested node generation to support rich text elements (bold, italic, code) within markdown content

---
+++ src/ssg_main.py
@@ -0,0 +1,95 @@
+from markdown_parser import text_to_textnodes, markdown_to_blocks
+from markdown_detector import block_to_block_type
+from parentnode import ParentNode
+from leafnode import LeafNode
+from textnode import TextNode, TextType
+import re
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+
+    for index, block in enumerate(blocks):
+        textnode_list = text_to_textnodes(block)
+        # determine block type
+        if block_to_block_type(textnode_list[0].text) == "normal":
+            paragraph_node = ParentNode(tag="p", value="", children=[])
+            for textnode in textnode_list:
+                node = text_to_children(textnode.text, textnode.text_type)
+                paragraph_node.children.append(node)
+            children.append(paragraph_node)
+        else:
+            for textnode in textnode_list:
+                if textnode.text_type == TextType.TEXT:
+                    text_type = block_to_block_type(textnode.text)
+                    if text_type == "normal":
+                        text_type = TextType.TEXT
+                    children.append(text_to_children(textnode.text, text_type))
+    return ParentNode(tag="div", children=children)
+
+def text_to_children(text, block_type):
+    if block_type == "heading":
+        return header_helper(text)
+    elif block_type == TextType.TEXT:
+        return normaltext_helper(text)
+    elif block_type == TextType.BOLD:
+        child_node = text_to_children(text, TextType.TEXT)
+        return ParentNode(tag="strong", value=text, children=child_node)
+    elif block_type == TextType.ITALIC:
+        child_node = text_to_children(text, TextType.TEXT)
+        return ParentNode(tag="em", value=text, children=child_node)
+    elif block_type == "unordered_list":
+        return unordered_helper(text)
+    elif block_type == "ordered_list":
+        return ordered_helper(text)
+    elif block_type == TextType.CODE:
+        return code_helper(text)
+    elif block_type == "quoteblock":
+        return blockquote_helper(text)
+    elif block_type == "codeblock":
+        return codeblock_helper(text)
+    else:
+        print(f"hit the end of text_to_children. block_type: {block_type}")
+
+def header_helper(text):
+    tag = f"h{text.count('#')}"
+    text = text.replace("#", "").strip()
+    child_node = [text_to_children(text, TextType.TEXT)]
+    return ParentNode(tag=tag, children=child_node)
+
+def normaltext_helper(text):
+    return(LeafNode(tag=None, value=text, props=None))
+
+def blockquote_helper(text):
+    text = text.replace(">", "").strip()
+    child_node = [text_to_children(text, TextType.TEXT)]
+    return ParentNode(tag="blockquote", children=child_node)
+
+def codeblock_helper(text):
+    text = text.replace("`", "").strip()
+    child_node = [text_to_children(text, TextType.TEXT)]
+    child_node = [ParentNode(tag="code", children=child_node)]
+    return ParentNode(tag="pre", children=child_node)
+
+def code_helper(text):
+    child_node = [text_to_children(text, TextType.TEXT)]
+    return ParentNode(tag="code", children=child_node)
+    
+def unordered_helper(text):
+    children = create_list_items(text)
+    return(ParentNode(tag="ul", children=children))
+
+def ordered_helper(text):
+    children = create_list_items(text)
+    return(ParentNode(tag="ol", children=children))
+
+def create_list_items(text):
+    # choose regex pattern based on whether list items belong to unordered "*" or ordered list
+    if text[0] == "*":
+        items_pattern = r'^\* (.+)'
+    else:
+        items_pattern = r'^\d+\. (.+)'
+    items = re.findall(items_pattern, text, re.MULTILINE)
+    return [ParentNode(tag="li", children=text_to_children(item, TextType.TEXT), props=None) for item in items]
+    
+
