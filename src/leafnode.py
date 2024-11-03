9332c278ba81f75fa321d517a7a7ea3dade6db35
mdelgadonyc <moises.f.delgado@gmail.com>
2024-11-02 20:01:43 -0400

    fix(LeafNode): Correct so that value isn't used if HTML tag is self-closing

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,5 +1,7 @@
 from htmlnode import HTMLNode
 
+SELF_CLOSING_TAGS = {"img", "br", "hr"}
+
 class LeafNode(HTMLNode):
     def __init__(self, value=None, tag=None, props=None) -> None:
         super().__init__(tag, value, None, props)
@@ -14,11 +16,12 @@ class LeafNode(HTMLNode):
         return props_string
     
     def to_html(self):
+        if self.tag in SELF_CLOSING_TAGS:
+            return f"<{self.tag}{self.props_to_html()}/>"
         if self.value == None:
             raise ValueError("Missing value")
         if self.tag == None:
             return self.value
         
         html_string = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-
         return html_string
