42b188ac12a53ce4e3db077b6788bdbe9e58537d
mdelgadonyc <moises.f.delgado@gmail.com>
2024-11-02 20:17:28 -0400

    feat(main): Enhance main by replacing the generation of one hardcoded markdown with handling as many as are found

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import shutil
 import re
 from ssg_main import markdown_to_html_node
 
-from_path = "content/index.md"
+from_path = "content/majesty/index.md"
 template_path = "template.html"
 dest_path = "public/index.html"
 
 def main():
     destination_dir = "public"
     source_dir = "static"
+
+    dir_path_content = "content"
 
     if os.path.exists(destination_dir) and os.path.exists(source_dir):
         print("Deleting and recreating public directory...")
@@ -18,7 +20,7 @@ def main():
 
         copy_files(source_dir, destination_dir)
     
-    generate_page(from_path, template_path, dest_path)
+    generate_pages_recursive(dir_path_content=dir_path_content, template_path=template_path, dest_dir_path=destination_dir)
                 
 def copy_files(src, dst):
     files = os.listdir(src)
@@ -39,6 +41,16 @@ def extract_title(markdown):
     title = title[2:]
     return title
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for root, dirs, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                dest_path = os.path.join(dest_dir_path, os.path.relpath(from_path, dir_path_content))
+                dest_path = os.path.splitext(dest_path)[0] + ".html"
+                generate_page(from_path, template_path, dest_path)
+
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
@@ -46,7 +58,6 @@ def generate_page(from_path, template_path, dest_path):
         markdown = f.read()
 
     title = extract_title(markdown)
-    print(f"Title: {title}")
 
     with open(template_path, "r") as f:
         template = f.read()
@@ -57,6 +68,10 @@ def generate_page(from_path, template_path, dest_path):
     html_content = html_node.to_html()
 
     template = template.replace("{{ Content }}", html_content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
     with open(dest_path, "w") as f:
         f.write(template)
